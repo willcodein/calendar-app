7f336e82c8465d5ba52b1a66cfd4089a9d7d1a83
William Taylor <willtaylor2@hotmail.com>
2018-03-14 23:14:28 +0000

    removed unnecessary spaces

--- src/components/dateDisplay/DateDisplay.js
+++ src/components/dateDisplay/DateDisplay.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './dateDisplay.css';
 
-
 const DateDisplay = ({ date }) => {
   return(
     <div className="date-display">
--- src/components/year/Year.js
+++ src/components/year/Year.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './year.css';
 
-
 const Year = ({ date, handleYear }) => {
   const yearsArr = ['2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'];
   let years = yearsArr.map(year => {
